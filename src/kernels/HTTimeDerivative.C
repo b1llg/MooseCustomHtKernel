195cffcced71ca6a640877b687946b08a7766ca1
b1llg <guyb1294@gmail.com>
2024-01-12 17:55:47 -0500

    Added time derivative kernel

---
+++ src/kernels/HTTimeDerivative.C
@@ -0,0 +1,46 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "HTTimeDerivative.h"
+
+#include "Material.h"
+
+registerMooseObject("MooseCustomHtKernelApp", HTTimeDerivative);
+
+InputParameters
+HTTimeDerivative::validParams()
+{
+  InputParameters params = TimeDerivative::validParams();
+    
+    params.addClassDescription("Computes the time derivative of temperature: $\\rho C_{p} \\dfrac{\partial T}{\partial t}$");
+
+    params.addRequiredParam<Real>("density", "density of the material kg/m3");
+    params.addRequiredParam<Real>("thermal_capacity", "thermal capacity j/kgk");
+
+    return params;
+}
+
+HTTimeDerivative::HTTimeDerivative(const InputParameters & parameters)
+  : TimeDerivative(parameters), 
+  _density(getParam<Real>("density")),
+  _thermalCapacity(getParam<Real>("thermal_capacity"))
+{
+}
+
+Real
+HTTimeDerivative::computeQpResidual()
+{
+  return _density * _thermalCapacity * TimeDerivative::computeQpResidual();
+}
+
+Real
+HTTimeDerivative::computeQpJacobian()
+{
+  return _density * _thermalCapacity * TimeDerivative::computeQpJacobian();
+}
